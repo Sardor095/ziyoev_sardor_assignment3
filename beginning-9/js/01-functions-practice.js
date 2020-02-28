42e20a2733a129be8f337a072cc474abd1033cb8
Sardor095 <60728653+Sardor095@users.noreply.github.com>
2020-02-28 13:58:54 -0800

    Add files via upload
    this is my assignment 3

---
+++ beginning-9/js/01-functions-practice.js
@@ -0,0 +1,35 @@
+//STEP 1
+// function halfNumber(num){
+//     var res = (num / 2);
+//     console.log(res);
+// }
+// halfNumber(120);
+//STEP 2
+// function squareNumber(num) {
+//     var res = (num * num);
+//     console.log(res);
+
+// }
+// squareNumber(90);
+//STEP 3
+// function percentOf(num1, num2){
+//     var res = (num1 / num2 * 100);
+//     console.log(res);
+//     return (res);
+// }
+// percentOf(3, 10);
+//STEP 4
+// function findModulus(num1, num2){
+//     var res = (num2 % num1);
+//     window.console.log(res + " is the module of " + num1 + " and " + num2);
+// }
+// findModulus(4, 10);
+
+//STEP 5
+// function delNum (sum1,sum2,sum3) {
+//     window.console.log("Calculated result is " + (Number(arguments[0]) + Number(arguments[1]) + Number(arguments[2])) + ".");
+// }
+// let sum1 = Number(window.prompt("Enter a number"));
+// let sum2  = Number(window.prompt("Enter another number"));
+// let sum3 = Number(window.prompt("One last number I promise!"));
+// delNum(sum1,sum2,sum3);
---
+++ beginning-9/js/02-fortune-teller.js
@@ -0,0 +1,11 @@
+function futureTeller(chil, part, pla, job) {
+    return window.console.log("You will be " + 
+    job + "in " + 
+    pla + ", and married to " + 
+    part + " with " + 
+    chil + "kids.")
+}
+
+futureTeller(2, "Brad Pitt", "Hawaii", "The GoodHacker ");
+futureTeller(3, "Andgelina Jolie", "Los Angelas", "The BadHacker ");
+futureTeller(4, "Adele", "London", "The 007 + Hacker ");
